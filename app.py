f51c8e83d234ba75378c7c5a92a4d1688fae051c
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-10-31 20:26:52 +0000

    Add analysis page and prediction page to app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_analysis_data import page_analysis_body
 from app_pages.page_project_hypotheses import page_hypothesis_o_validation_body
+from app_pages.page_predictore import ML_prediction_exam_score
 from app_pages.page_ml_student_exam_score_predictore import page_ML_predict_student_exam_body
 
 
@@ -12,8 +14,9 @@ app = MultiPage(app_name="Exam Score Predictor")
 
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Project Hypothesis and Validation", page_hypothesis_o_validation_body)
-app.add_page("Exploratory Analysis of the Data", page_ML_predict_student_exam_body)
-# app.add_page("ML: Predict Success", page_ml_success_predictor_body)
-# app.add_page("IVF Success Predictor", page_live_predictor_body)
+app.add_page("Exploratory Analysis of the Data", page_analysis_body)
+app.add_page("Prediction of Student Exam Score", ML_prediction_exam_score)
+app.add_page("ML: Predict Exam score", page_ML_predict_student_exam_body)
+
 
 app.run()
---
+++ app_pages/page_analysis_data.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def page_analysis_body():
+    st.success(
+        """
+        Add text here
+        """
+    )
