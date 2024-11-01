8c5d8db7357ddce3b1a2510eeaeda787f0f670ee
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-11-01 13:41:36 +0000

    Add conclusion to hyothersis 1

--- app_pages/page_project_hypotheses.py
+++ app_pages/page_project_hypotheses.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def page1_body():
+def page_hypothesis_o_validation_body():
     st.write(
         "This is the first page"
     )
@@ -14,17 +14,26 @@ def page1_body():
     st.warning(
         """
         #### Conclusion
+        The data indicates that there in a trende that shows of 
+        the student having a higher changes of getting a higher Exam
+        Score if they a higher attendance and houres stuidied. But 
+        there are nothigh that support the statmate the student 
+        will have a higher change to get a score that is higher 
+        then 75.
+
+        This will conclude that this hypothesis will be rejected
         """
     )
     st.success(
         """
         ## Hypothesis 2
-        he education level of the students' parents does not have a significant impact on the students' final exam scores.
+        The education level of the students' parents does not have a significant impact on the students' final exam scores.
         """
     )
     st.warning(
         """
         #### Conclusion
+        
         """
     )
     st.success(
