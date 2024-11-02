ebd21c92868978d46c68d1b697c130c639075dde
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-11-02 09:01:40 +0000

    Add functions to show correlation plots

--- src/machine_learning/plot_functions.py
+++ src/machine_learning/plot_functions.py
@@ -1,6 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import ppscore as pps
 import matplotlib.patheffects as path_effects
 import matplotlib.pyplot as plt
-import seaborn as sns
+
+
 
 def add_median_labels(ax: plt.Axes, fmt: str = ".1f") -> None:
     """Add text labels to the median lines of a seaborn boxplot.
@@ -27,3 +33,45 @@ def add_median_labels(ax: plt.Axes, fmt: str = ".1f") -> None:
             path_effects.Stroke(linewidth=1, foreground=median.get_color()),
             path_effects.Normal(),
         ])
+
+
+
+def heatmap_corr(df, threshold, figsize=(20, 12), font_annot=8):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool_)
+        mask[np.triu_indices_from(mask)] = True
+        mask[abs(df) < threshold] = True
+
+        fig, axes = plt.subplots(figsize=figsize)
+        sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                    mask=mask, cmap='viridis', annot_kws={"size": font_annot}, ax=axes,
+                    linewidth=0.5
+                    )
+        axes.set_yticklabels(df.columns, rotation=0)
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
+
+
+def heatmap_pps(df, threshold, figsize=(20, 12), font_annot=8):
+    if len(df.columns) > 1:
+        mask = np.zeros_like(df, dtype=np.bool_)
+        mask[abs(df) < threshold] = True
+        fig, ax = plt.subplots(figsize=figsize)
+        ax = sns.heatmap(df, annot=True, xticklabels=True, yticklabels=True,
+                         mask=mask, cmap='rocket_r', annot_kws={"size": font_annot},
+                         linewidth=0.05, linecolor='grey')
+        plt.ylim(len(df.columns), 0)
+        st.pyplot(fig)
+
+
+def CalculateCorrAndPPS(df):
+    df_corr_spearman = df.corr(method="spearman", numeric_only=True)
+    df_corr_pearson = df.corr(method="pearson", numeric_only=True)
+    
+    pps_matrix_raw = pps.matrix(df)
+    pps_matrix = pps_matrix_raw.filter(['x', 'y', 'ppscore']).pivot(columns='x', index='y', values='ppscore')
+
+    pps_score_stats = pps_matrix_raw.query("ppscore < 1").filter(['ppscore']).describe().T
+
+    return df_corr_pearson, df_corr_spearman, pps_matrix
+
